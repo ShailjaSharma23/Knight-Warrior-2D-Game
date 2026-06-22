74164d4f475ea665502cdb95136f48a02e8b9cb9
Shailja Sharma <shailjasharma2307@gmail.com>
2026-06-22 11:49:38 +0530

    Melee enemy and its animations

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -14,10 +14,14 @@ void OnTriggerEnter2D(Collider2D collision)
         if (collision.transform.position.x < transform.position.x)
         {
             cam.MoveToNewRoom(nextRoom);
+            nextRoom.GetComponent<Room>().ActivateRoom(true);
+            prevRoom.GetComponent<Room>().ActivateRoom(false);
         }
         else
         {
             cam.MoveToNewRoom(prevRoom);
+            prevRoom.GetComponent<Room>().ActivateRoom(true);
+            nextRoom.GetComponent<Room>().ActivateRoom(false);
         }
     }
 }
