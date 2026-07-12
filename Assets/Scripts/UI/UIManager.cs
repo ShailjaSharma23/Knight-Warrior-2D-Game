db465a25177b5a067cd7342fb1bd4b2f4935007e
Shailja Sharma <shailjasharma2307@gmail.com>
2026-07-12 16:34:05 +0530

    Pause menu and options

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            Debug.Log("Escape Pressed");
+            if(pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+        }
+    }
+
+    #region Game Over
 
     public void GameOver()
     {
@@ -30,6 +50,36 @@ public class UIManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+
+        #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
+
+    #endregion
+
+    #region Pause
+
+    public void PauseGame(bool status)
+    {
+        
+        pauseScreen.SetActive(status);
+
+        if (status)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    public void SoundVolume()
+    {
+        SoundManager.instance.changeSoundVol(0.2f);
+    }
+
+    public void MusicVolume()
+    {
+        SoundManager.instance.changeMusicVol(0.2f);
+    }
+
+    #endregion
 }
